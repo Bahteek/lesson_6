817a162619fb11822b5ea0bde2efe5f2bda89524
Bahteek <148778390+Bahteek@users.noreply.github.com>
2023-11-08 16:38:14 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,100 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+//   int vozrast = 16;
+//   int a = 18;
+//   if (vozrast>=a)
+//   { System.out.println(vozrast+" " + "Tapsyra alady");
+//        }else{System.out.println("Tapsyra almaydy");}
+//    }
+
+//        int baga = 2;
+//        int a = 2;
+//        if (a != baga) {
+//            System.out.println("Kelesi synypka ote alady");
+//        } else {
+//            System.out.println("Ote almaidy");
+//        }
+//        int x = 23;
+//        int y = 23;
+//        if (x > y) {
+//            System.out.println("x больше чем y");
+//        }
+//        if (x < y) {
+//            System.out.println("x меньше чем y");
+//        } else {
+//            System.out.println("x и y равны");
+//        }
+//        int kurs = 5;
+//        if (kurs == 1) {
+//            System.out.println("Birinshi kursta:Matematika, Fizika, Psihologya okytylady");
+//        } else if(kurs ==2) {
+//            System.out.println("Ekinshi kursta:VyshMat, Tarih, Informaitka okytylady");
+//        } else if (kurs == 3) {
+//            System.out.println("Ushinsi kursta:Prec sabaktar okytylady");
+//        } else if (kurs == 4) {
+//            System.out.println("Tortinshi kursta diplomdyk jumis");
+//
+//        } else {
+//            System.out.println("Oku tek 1-4 kurs aralygynda");
+
+
+//        int ball = 1;
+//        if(ball >= 0 && ball <=49) {
+//            System.out.println("Algan bagasy 2");
+//        }
+//        else if (ball >= 51 && ball <= 69){
+//            System.out.println("Algan bagasy 3");
+//        } else if (ball >= 71 && ball <=84) {
+//            System.out.println("Algan bagasy 4");
+//
+//        } else if (ball >= 86 && ball <= 100) {
+//            System.out.println("Algan bagasy 5");
+//
+//        } else {
+//            System.out.println("Invalid");
+//        }
+        /* char kosu = '+';
+        char azaytu = '-';
+        char kobeitu = '*';
+        char bolu = '/';
+        char final_char = '/';
+        int n = 2;
+        int m =5;
+        if (final_char == kosu) {
+            System.out.println(n + m);
+        } else if (final_char == azaytu) {
+            System.out.println(n - m);
+        } else if (final_char == kobeitu) {
+            System.out.println(n * m);
+        } else {
+            if (m == 0) {
+                System.out.println("Санды нолге тенестиру дурыс емес");
+            } else {
+                if ( n== 0) {
+                    System.out.println("Санды нолге тенестиру дурыс емес");
+                } else {
+                    System.out.println(n / m);
+                } */
+
+        /*char course_name = 'F';
+        int course_chislo = 3;
+        if (course_name == 'F') {
+
+            if (course_chislo == 1) {
+                System.out.println("Окушы саны 30");
+            } else if (course_name == 'F') if (course_chislo == 2) {
+                System.out.println("Окушы саны 25");
+            } else if (course_name == 'F') {
+                if (course_chislo == 3) {
+                    System.out.println("Окушы саны 20");
+                }
+            }
+       */ }
+    }
+}
+
+
+
+
